acc8b3ec72a2c9d737e187669076de8ca2bfb932
kedinha <kedmyknow@gmail.com>
2022-06-23 20:00:24 -0300

    Criando calculadora

--- sintaxeBasica/Aula6/script.js
+++ sintaxeBasica/Aula6/script.js
@@ -1,8 +1,23 @@
 // aula 6 - aprofundando em funções
 // tipos de funções
 
-//declarativa - precisa de uma nome
-function name() {
+//declarativa - precisa de um nome
+/* function funcao() {
   console.log('Sou uma função declrativa')
 }
-name()
+funcao()
+ */
+
+//Expressões defunções e a nomeação é opcional
+
+/* var funcao = function () {
+  console.log('Sou uma função de expressão')
+}
+funcao() */
+
+//Arrow function expressão de sintaxe curta
+
+/* var funcao = () => {
+  console.log('Sou uma arrow function')
+}
+funcao() */
