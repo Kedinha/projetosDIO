a0561e01cd5fe20b98c35445f9c312dd823758a9
kedinha <kedmyknow@gmail.com>
2023-12-10 17:10:06 -0300

    Validando métodos

--- DominandoJava/quick-start-maven/src/test/java/ked/digitalinnovation/AppTest.java
+++ DominandoJava/quick-start-maven/src/test/java/ked/digitalinnovation/AppTest.java
@@ -8,10 +8,10 @@ import org.springframework.boot.test.system.OutputCaptureExtension;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(OutputCaptureExtension.class)
-public class AppTest {
+class AppTest {
 
     @Test
-    void testHello(CapturedOutput capture) throws Exception {
+    void hello(CapturedOutput capture) throws Exception {
         App.main(new String[]{});
         assertThat(capture.toString()).isEqualToIgnoringNewLines("Hello World!");
     }
