24d41f7d4e1c77cbb6bd53a81f71500d0ecf3f08
kedinha <kedmyknow@gmail.com>
2024-01-06 23:17:59 -0300

    update TS

--- SantanderBootcamp/IntroTS/src/intefaces.ts
+++ SantanderBootcamp/IntroTS/src/intefaces.ts
@@ -13,6 +13,8 @@ readonly  id: number | string;
 interface robot2 {
 readonly  id: number | string,
   name: string
+  sayHello(): string;
+
 }
 
 const bot: robot = {
@@ -20,10 +22,30 @@ const bot: robot = {
   name: "megaman"
 }
 const bot2: robot2 = {
-  id:"1",
-  name: "megaman"
+  id: "1",
+  name: "megaman",
+  sayHello: function (): string {
+    throw new Error("Function not implemented.");
+  }
 }
 
 console.log(bot);
 console.log(bot2);
 
+class Pessoa implements robot2 {
+  id: string | number;
+  name: string;
+
+  constructor(id: string | number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+
+  sayHello(): string {
+   return `Hello I'm ${this.name}`;
+    
+  }
+}
+
+const p = new Pessoa(1, "gustman")
+console.log(p.sayHello());
