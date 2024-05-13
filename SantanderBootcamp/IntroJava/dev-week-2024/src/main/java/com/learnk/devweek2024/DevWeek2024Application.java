c616fd62aab7dfefe401fc4f0fddafc60ae8ce99
kedinha <kedmyknow@gmail.com>
2024-05-13 15:10:38 -0300

    add IntroJava

---
+++ SantanderBootcamp/IntroJava/dev-week-2024/src/main/java/com/learnk/devweek2024/DevWeek2024Application.java
@@ -0,0 +1,13 @@
+package com.learnk.devweek2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWeek2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevWeek2024Application.class, args);
+	}
+
+}
---
+++ SantanderBootcamp/IntroJava/dev-week-2024/src/test/java/com/learnk/devweek2024/DevWeek2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnk.devweek2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevWeek2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
