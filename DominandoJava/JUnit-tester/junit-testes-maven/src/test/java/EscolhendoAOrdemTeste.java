83c4650a1bafbe0f99e5d80379020a3fe239ae28
kedinha <kedmyknow@gmail.com>
2024-05-17 16:18:59 -0300

    Teste DisplayName

--- DominandoJava/JUnit-tester/junit-testes-maven/src/test/java/EscolhendoAOrdemTeste.java
+++ DominandoJava/JUnit-tester/junit-testes-maven/src/test/java/EscolhendoAOrdemTeste.java
@@ -1,26 +1,33 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 // @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 // @TestMethodOrder(MethodOrderer.MethodName.class)
-@TestMethodOrder(MethodOrderer.Random.class)
+// @TestMethodOrder(MethodOrderer.Random.class)
+@TestMethodOrder(MethodOrderer.DisplayName.class)
 public class EscolhendoAOrdemTeste {
 
   // @Order(3)
+  @DisplayName("A")
   @Test
   void validaFluxoA() {
     Assertions.assertTrue(true);
   }
 
   // @Order(1)
+  @DisplayName("B")
+
   @Test
   void validaFluxoB() {
     Assertions.assertTrue(true);
   }
 
   // @Order(2)
+  @DisplayName("C")
+
   @Test
   void validaFluxoC() {
     Assertions.assertTrue(true);
