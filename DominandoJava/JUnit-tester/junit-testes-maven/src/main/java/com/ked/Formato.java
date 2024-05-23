6c9909a7c0d18bd1a278d77c4344a4f427d2aa01
kedinha <kedmyknow@gmail.com>
2024-05-23 02:23:00 -0300

    Add Test Spy  Verifica Mensagem

---
+++ DominandoJava/JUnit-tester/junit-testes-maven/src/main/java/com/ked/Formato.java
@@ -0,0 +1,5 @@
+package com.ked;
+
+public enum Formato {
+  HTML, TEXTO;
+}
---
+++ DominandoJava/JUnit-tester/junit-testes-maven/src/test/java/EnviarMensagemTeste.java
@@ -0,0 +1,31 @@
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.ked.EnviarMensagem;
+import com.ked.Mensagem;
+
+@ExtendWith(MockitoExtension.class)
+
+public class EnviarMensagemTeste {
+
+  @Spy
+  private EnviarMensagem enviarMensagem = new EnviarMensagem();
+
+  @Test
+  void verificarComportamentoDaClasse() {
+    Mockito.verifyNoInteractions(enviarMensagem);
+
+    Mensagem mensagem = new Mensagem("Hello world!");
+    enviarMensagem.adicionarMensagem(mensagem);
+
+    Mockito.verify(enviarMensagem).adicionarMensagem(mensagem);
+
+    Assertions.assertFalse(enviarMensagem.getMensagens().isEmpty());
+  }
+}
