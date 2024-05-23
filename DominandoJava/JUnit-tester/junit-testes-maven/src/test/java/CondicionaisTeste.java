cbbe53e2235b505df20317f0eba20927f248bfc6
kedinha <kedmyknow@gmail.com>
2024-05-22 23:26:37 -0300

    test lines coments

--- DominandoJava/JUnit-tester/junit-testes-maven/src/test/java/CondicionaisTeste.java
+++ DominandoJava/JUnit-tester/junit-testes-maven/src/test/java/CondicionaisTeste.java
@@ -1,11 +1,11 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
-import org.junit.jupiter.api.condition.EnabledForJreRange;
+// import org.junit.jupiter.api.condition.EnabledForJreRange;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
-import org.junit.jupiter.api.condition.EnabledOnJre;
+// import org.junit.jupiter.api.condition.EnabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.JRE;
+// import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.condition.OS;
 
 public class CondicionaisTeste {
