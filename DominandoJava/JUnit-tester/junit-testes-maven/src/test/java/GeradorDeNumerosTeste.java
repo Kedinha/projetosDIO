c5bf7e76d0256e17bb30b5de48e500b4a728f2b4
kedinha <kedmyknow@gmail.com>
2024-06-10 18:12:37 -0300

    Mockito inline para testar métodos estátivos

---
+++ DominandoJava/JUnit-tester/junit-testes-maven/src/test/java/GeradorDeNumerosTeste.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.ked.GeradorDeNumeros;
+
+@ExtendWith(MockitoExtension.class)
+public class GeradorDeNumerosTeste {
+
+  @Test
+  void testaGeracaoComTamanhoDefinido() {
+
+    MockedStatic<GeradorDeNumeros> mockedStatic = Mockito.mockStatic(GeradorDeNumeros.class);
+  }
+
+}
